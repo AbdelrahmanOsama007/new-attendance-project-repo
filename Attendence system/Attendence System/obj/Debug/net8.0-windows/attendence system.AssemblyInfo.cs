6e95f39d9eb156717c42d4859b15c01ab0a18cfc
Ahmad Esam <90632545+Ahmad-Esam12398@users.noreply.github.com>
2024-02-20 21:29:48 +0200

    llll

--- Attendence system/Attendence System/obj/Debug/net8.0-windows/attendence system.AssemblyInfo.cs
+++ Attendence system/Attendence System/obj/Debug/net8.0-windows/attendence system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("attendence system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d048a32a8ed02ddf53f431ab0c2c760a16e0505")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecf846a6026afc863280a005df748d10ed0ad2d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("attendence system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("attendence system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
